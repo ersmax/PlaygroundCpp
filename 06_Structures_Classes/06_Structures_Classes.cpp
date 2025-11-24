1d7ae9c3d8be1eab948afbcd685ec5413f8cc9e6
ersmax <e.masarin@student.vu.nl>
2025-11-24 14:18:32 +0100

    Add project files.

---
+++ 06_Structures_Classes/06_Structures_Classes.cpp
@@ -0,0 +1,12 @@
+﻿// 06_Structures_Classes.cpp : Defines the entry point for the application.
+//
+
+#include "06_Structures_Classes.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
