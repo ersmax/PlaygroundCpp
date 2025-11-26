6568f356f7b4da64c104c4401071a0361bd959ed
ersmax <masarin.eros@gmail.com>
2025-11-26 16:12:11 +0100

    Add CD and function double interest

---
+++ 06_Structures_Classes/01_Structures/6_0_2_Certificate_Deposit_Dates.cpp
@@ -0,0 +1,18 @@
+//  This program demonstrates the use of Hierarchical Structures 
+//  in a banking programme that computes interests on a bank 
+//  certificate deposit that does not allow withdrawals.
+
+#include <iostream>
+
+struct Date
+{
+	int month;
+	int day;
+	int year;
+};
+
+struct CDAccount
+{
+	double initialBalance;
+
+};
