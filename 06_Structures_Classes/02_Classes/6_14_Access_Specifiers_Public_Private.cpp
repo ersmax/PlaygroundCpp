04b593847b9961a36a976430822b206be18f2ce7
ersmax <e.masarin@student.vu.nl>
2025-11-29 00:08:29 +0100

    Add Pitfalls and explanations

---
+++ 06_Structures_Classes/02_Classes/6_14_Access_Specifiers_Public_Private.cpp
@@ -0,0 +1,17 @@
+// Explain what public: and private: mean in a class definition.
+
+// Private members (member function or member variable) can only
+// be accessed in the definition of a member function of the class
+// which they belong to.
+// Public members can be accessed anywhere.
+
+// Formally:
+//    All members(member variables and member functions) that are marked private:
+// can only be accessed by name in the definitions of member functions(both public
+// and private) of the same class.Members marked public: have no restrictions on
+// where they can be used.
+
+int main( )
+{
+	return 0;
+}
---
+++ 06_Structures_Classes/02_Classes/6_15_Required_Public_Private_Sections.cpp
@@ -0,0 +1,14 @@
+//a.How many public: sections are required in a class for the class to be useful ?
+//b.How many private: sections are required in a class ?
+
+// a. At least one public section
+// b. None required, but at least one recommended for class member variables 
+// Formally:
+// a.Only one.The compiler warns if you have no public: members in a class (or
+//	 struct, for that matter).
+// b.None, but we normally expect to find at least one private: section in a class.
+
+int main( )
+{
+	return 0;
+}
