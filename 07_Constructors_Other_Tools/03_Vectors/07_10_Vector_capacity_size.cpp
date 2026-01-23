d5c22543a55c341d5a423506e9328937ba3a7438
ersmax <masarin.eros@gmail.com>
2026-01-23 18:31:48 +0100

    add vector capacity and size

---
+++ 07_Constructors_Other_Tools/03_Vectors/07_10_Vector_capacity_size.cpp
@@ -0,0 +1,11 @@
+// What is the difference between the size and the capacity of a vector?
+// Size = n elements of a vector
+// Capacity = n elements for which the vector currently has memory allocated
+
+#include <iostream>
+
+int main( )
+{
+	std::cout << '\n';
+	return 0;
+}
