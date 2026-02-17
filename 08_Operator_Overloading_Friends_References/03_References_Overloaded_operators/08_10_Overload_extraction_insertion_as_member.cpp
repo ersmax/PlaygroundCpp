0f4c6eda630d9e2d52c09acdc43a44a5c4e6cf08
ersmax <masarin.eros@gmail.com>
2026-02-17 22:56:00 +0100

    add << and >> overload as members

---
+++ 08_Operator_Overloading_Friends_References/03_References_Overloaded_operators/08_10_Overload_extraction_insertion_as_member.cpp
@@ -0,0 +1,17 @@
+//Why can we not overload << or >> as member operators ?
+
+// Because if we did, we would not be able to use them the way we intend to.
+// This is because the first member (the calling object) is an Abstract Data Type (i.e. a class),
+// and not of type iostream, as opposed to overloading the insertion of extraction
+// as non-member non friend functions, or non-member friend functions.
+
+// Formally:
+// If << and >> are to work as we want, then the first operand(first argument) must
+// be cout or cin(or some file I / O stream).But if we want to overload the operators
+// as members of, say, the class Money, then the first operand would have to be the
+// calling object and so would have to be of type Money, and that is not what we want.
+
+int main( )
+{
+	return 0;
+}
