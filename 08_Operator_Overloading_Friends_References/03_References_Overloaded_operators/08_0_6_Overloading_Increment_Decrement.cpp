35140011f56d3de8b690d763b708c56e1e7b87f9
ersmax <masarin.eros@gmail.com>
2026-02-18 20:05:59 +0100

    add overloading of increment and decrement operators

---
+++ 08_Operator_Overloading_Friends_References/03_References_Overloaded_operators/08_0_6_Overloading_Increment_Decrement.cpp
@@ -0,0 +1,61 @@
+//   This program showcases the overload of the increment and decrement operators
+// both in prefix notation (++a) and postfix notation (a++)
+
+#include <iostream>
+#include <cstdlib>
+
+class IntPair
+{
+public:
+	IntPair(const int firstValue, const int secondValue) { setFirst(firstValue); setSecond(secondValue); }
+	IntPair& operator ++();					// Prefix notation (returns by reference)
+	const IntPair operator ++(int);			// Postfix notation	(returns by value)
+	void setFirst(const int newValue) { first = newValue; }
+	void setSecond(const int newValue) { second = newValue; }
+	int getFirst() const { return first; }
+	int getSecond() const { return second; }
+private:
+	int first;
+	int second;
+};
+
+int main( )
+{	
+	IntPair a(1, 2);
+	std::cout << "Postfix a++\n"
+			  << "Start value of a: ";
+	std::cout << a.getFirst() << " " << a.getSecond() << '\n';
+	const IntPair b = a++;
+	std::cout << "Value returned: ";
+	std::cout << b.getFirst() << " " << b.getSecond() << '\n';
+	std::cout << "Changed object: ";
+	std::cout << a.getFirst() << " " << a.getSecond() << '\n';
+
+	a = IntPair(1, 2);
+	std::cout << "Prefix ++a\n"
+			  << "Start value of a: ";
+	std::cout << a.getFirst() << " " << a.getSecond() << '\n';
+	const IntPair c = ++a;
+	std::cout << "Value returned: ";
+	std::cout << c.getFirst() << " " << c.getSecond() << '\n';
+	std::cout << "Changed object: ";
+	std::cout << a.getFirst() << " " << a.getSecond() << '\n';
+
+	std::cout << '\n';
+	return 0;
+}
+
+IntPair& IntPair::operator ++()
+{
+	first++;
+	second++;
+	return *this;
+}
+
+const IntPair IntPair::operator ++(int)
+{
+	const IntPair temp(first, second);
+	first++;
+	second++;
+	return temp;
+}
