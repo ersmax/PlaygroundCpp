d7b37bfaa39cbfc2c459e7543e488fb965665890
ersmax <masarin.eros@gmail.com>
2026-02-23 23:50:55 +0100

    add destroy dynamic variable

---
+++ 10_Pointers_Dynamic_arrays/01_Pointers/10_06_Destroy_Dynamic_variable.cpp
@@ -0,0 +1,16 @@
+/*
+Suppose a dynamic variable were created as follows:
+char *p;
+p = new char;
+Assuming that the value of the pointer variable p has not changed (so it still
+points to the same dynamic variable), how can you destroy this new dynamic
+variable and return the memory it uses to the freestore manager so that the
+memory can be reused to create other new dynamic variables?
+*/
+
+// delete p;
+
+int main( )
+{
+	return 0;
+}
